057f8b4b53840c5204b4eb6aa473c972f2aa0531
SystemEdbar <systemedbar@gmail.com>
2021-10-09 12:53:18 -0600

    Configuracion del Router User

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
-import { NativeScriptModule } from '@nativescript/angular'
+import { NgModule } from '@angular/core'
+import { Routes } from '@angular/router'
+import { NativeScriptRouterModule } from '@nativescript/angular'
+
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { ItemsComponent } from './item/items.component'
 import { ItemDetailComponent } from './item/item-detail.component'
+import { UserComponent } from './user/user.component'
 
 @NgModule({
   bootstrap: [AppComponent],
