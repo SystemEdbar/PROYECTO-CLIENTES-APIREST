82f52208bb3c3efc2bc2ae1b4a536f2277013a73
SystemEdbar <systemedbar@gmail.com>
2021-10-12 21:37:12 -0600

    Se Crearon los archivos componentes y los nuevos routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import { NativeScriptRouterModule } from '@nativescript/angular'
 import { ItemsComponent } from './item/items.component'
 import { ItemDetailComponent } from './item/item-detail.component'
 import { UserComponent } from './user/user.component'
+import { UserCreateComponent } from './user/usercreate.component'
+import { UserEditComponent } from './user/useredit.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/items', pathMatch: 'full' },
   { path: 'items', component: ItemsComponent },
   { path: 'item/:id', component: ItemDetailComponent },
-  { path: 'user' component: UserComponent},
+  { path: 'users' component: UserComponent},
+  { path: 'user/edit' component: UserEditComponent},
+  { path: 'user/create' component: UserCreateComponent},
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component'
 import { ItemsComponent } from './item/items.component'
 import { ItemDetailComponent } from './item/item-detail.component'
 import { UserComponent } from './user/user.component'
+import { UserCreateComponent } from './user/usercreate.component'
+import { UserEditComponent } from './user/useredit.component'
 
 @NgModule({
   bootstrap: [AppComponent],
   imports: [NativeScriptModule, AppRoutingModule],
-  declarations: [AppComponent, ItemsComponent, ItemDetailComponent, UserComponent],
+  declarations: [AppComponent, ItemsComponent, ItemDetailComponent, UserComponent, UserCreateComponent, UserEditComponent],
   providers: [],
   schemas: [NO_ERRORS_SCHEMA],
 })
