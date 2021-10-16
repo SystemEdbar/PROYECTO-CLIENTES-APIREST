b32c5f6cd5f4546853c01bd62d707a0bc3d955eb
Bams <bmorenos@miumg.edu.gt>
2021-10-16 01:29:01 -0500

    Lista de Usuarios

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { platformNativeScript, runNativeScriptAngularApp } from '@nativescript/angular';
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
 import { AppModule } from './app/app.module'
 
